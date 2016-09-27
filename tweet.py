459c2f33a811a4445720dc6b7cc17251a2c37c85
William Knowles <will@hollyandwill.com>
2016-09-27 17:26:09 -0400

    local deploy

--- tweet.py
+++ tweet.py
@@ -2,12 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import tweepy
-
-# import auth from heroku
-CONSUMER_KEY = os.environ['CONSUMER_KEY']
-CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
+from secrets import *
 
 # twitter auth
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -15,5 +10,26 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 
+# override tweepy.StreamListener to add logic to on_status
+class MyStreamListener(tweepy.StreamListener):
 
-api.update_status()
+    def on_status(self, status):
+        print(status.user.screen_name, status.text)
+        api.update_status(".@" + status.user.screen_name + " Then I don't need a jacket!", in_reply_to_status_id=status.id)
+
+    # error handling
+    def on_error(self, status_code):
+        if status_code == 420:
+            # returning False in on_data disconnects the stream
+            return False
+
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+
+myStream.filter(track=["Pretty much everywhere, it's gonna be hot."])
+
+# favorite all replies
+# if @reply:
+# get @reply tweet id
+#   api.create_favorite(id)
